634bac198fbca249ccf798f5364249d2a0bb21c3
sschoe85 <s@bine-schoetensack.eu>
2020-08-24 14:30:07 +0200

    User Input read or write

--- cli.js
+++ cli.js
@@ -1,35 +1,53 @@
 const inquirer = require("inquirer");
 const fs = require("fs").promises;
+
+const masterPassword = [
+  {
+    type: "password",
+    name: "password",
+    message: "Please enter your master password",
+  },
+];
+
+const userInput = [
+  {
+    type: "input",
+    name: "userInput",
+    message: "Please enter 'write' oder 'read'",
+  },
+];
+function getReadOrWrite(){
+inquirer.prompt(userInput).then(async (answers) => {
+  if (answers.userInput === "read") {
+    try {
+      const passwordsJSON = await getPasswords();
+      const passwords = JSON.parse(passwordsJSON);
+      console.log("Your password is: " + passwords);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  if (answers.userInput === "write"){
+    
+  }
+  else{
+    console.log("Error: 'read' oder 'write' are the only values accepted");
+  }
+});
+}
 async function getPasswords() {
   const pwFile = await fs.readFile("./passwords.json", "utf8");
   return pwFile;
 }
 
-
-const myPassword = [
-  {
-    type: "password",
-    name: "password",
-    message: "Please enter your password",
-  },
-  {
-    type: "input",
-    name: "key",
-    message: "Which password do you need?",
-  },
-];
-
-inquirer.prompt(myPassword).then(async (answers) => {
-  if (answers.password === "123") {
-    try{
-    const passwordsJSON = await getPasswords();
-    const passwords = JSON.parse(passwordsJSON);
-    console.log("Your password is: " + passwords[answers.key]);
+function askForMasterPassword(){
+  inquirer.prompt(masterPassword).then(async (answers) => {
+    if (answers.password === "master") {
+      getReadOrWrite();
+    } else {
+      console.log("wrong password");
     }
-    catch(error){
-      console.log(error);
-    }
-  } else {
-    console.log("wrong password");
-  }
-});
+  });
+}
+
+askForMasterPassword();
