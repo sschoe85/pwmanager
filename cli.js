8dff038dac702a64a06817b02f3fbd4edcf94ed8
sschoe85 <s@bine-schoetensack.eu>
2020-08-24 11:51:12 +0200

    PW Manager VS 1

--- cli.js
+++ cli.js
@@ -1,13 +1,28 @@
 const inquirer = require("inquirer");
+const fs = require("fs").promises;
+async function getPasswords() {
+  const pwFile = await fs.readFile("./passwords.json", "utf8");
+  return pwFile;
+}
 
 const myPassword = [
   {
     type: "password",
-    name: "mypw",
+    name: "password",
     message: "Please enter your password",
+  },
+  {
+    type: "input",
+    name: "key",
+    message: "Which password do you need?",
   },
 ];
 
-inquirer.prompt(myPassword).then((pw) => {
-  console.log(`Thank you, you will be hacked!`);
+inquirer.prompt(myPassword).then(async (answers) => {
+  if (answers.password === "123") {
+    const data = await getPasswords();
+    console.log(data);
+  } else {
+    console.log("wrong password");
+  }
 });
